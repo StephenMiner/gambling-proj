34d5813497bea042f8bc0af69912ec64e5660493
Meep <stephen.archer.j@gmail.com>
2026-06-07 21:32:55 -0700

    Add slot cost static var

--- components/root.gd
+++ components/root.gd
@@ -2,6 +2,7 @@ class_name Root;
 extends Node2D
 
 static var money : int = 200;
+static var cost : int = 10;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
