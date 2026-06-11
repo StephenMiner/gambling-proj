85a1fc5ff72c452425d6ef9561b47b1c831f9e80
Meep <stephen.archer.j@gmail.com>
2026-06-11 09:53:12 -0700

    Add backgrounds

---
+++ components/background.gd
@@ -0,0 +1,67 @@
+extends Sprite2D
+
+enum States {NEUTRAL, HAPPY, MAD};
+
+var state : States;
+var default_state : States;
+var revert : bool = false;
+
+var duration : float;
+var time_elapsed : float;
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	default_state = States.NEUTRAL;
+	state = default_state;
+	update_image();
+	var coin = get_parent().get_node("Sprite2D");
+	coin.money_change.connect(money_check);
+	self.owner.pay_bld.connect(blood_check);
+	pass # Replace with function body.
+
+func money_check()->void:
+	if (Root.money > 0 and state != States.NEUTRAL):
+		default_state = States.NEUTRAL;
+		state = default_state;;
+		
+	if (Root.money <= 0 and state != States.MAD):
+		default_state = States.MAD;
+		state = default_state;
+		revert = false;
+		update_image();
+
+func blood_check()->void:
+	revert = true;
+	duration = 4;
+	time_elapsed = 0;
+	if (state == States.HAPPY):
+		return;
+	state = States.HAPPY
+	update_image();
+
+func roll_check()->void:
+	time_elapsed = 0;
+	duration = 8;
+	revert = true;
+	if (state == States.HAPPY):
+		return;
+	state == States.HAPPY;
+	update_image();
+
+func update_image()->void:
+	if state == States.NEUTRAL:
+		self.texture = load("res://icons/pretty_neutral.png");
+	elif state == States.HAPPY:
+		self.texture = load("res://icons/pretty_happy.png");
+	elif state == States.MAD:
+		self.texture = load("res://icons/pretty_angry.png");
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if revert:
+		if (time_elapsed > duration):
+			revert = false;
+			state = default_state;
+			time_elapsed = 0;
+			update_image();
+		time_elapsed += delta;
+	pass
---
+++ components/end_screen.gd
@@ -0,0 +1,48 @@
+extends ColorRect
+
+
+
+enum Ending {DEBT, DEAD};
+
+var label : Label;
+var current : Ending;
+var end : bool;
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var screen_size : Vector2  = get_viewport().get_visible_rect().size;
+	self.size = screen_size;
+	self.position = (screen_size / 2) - (self.size / 2);
+	self.set_color(Color(0.3, 0, 0.3));
+	self.end = false;
+	self.label = get_node("Label");
+	self.hide();
+	self.label.hide();
+	self.label.global_position = (screen_size / 4);
+	
+	var root : Node2D = get_parent();
+	root.pay_bld.connect(blood_listen);
+	root.get_node("Sprite2D").money_change.connect(money_listen);
+	pass # Replace with function body.
+
+
+func blood_listen()->void:
+	if Root.blood_spilt > 20: 
+		set_ending(Ending.DEAD);
+func money_listen()->void:
+	if (Root.money < -20):
+		set_ending(Ending.DEBT);
+
+func set_ending(ending : Ending)->void:
+	self.current = ending;
+	self.end = true;
+	if (ending == Ending.DEBT):
+		self.set_color(Color(0.3, 0, 0.3));
+		label.text = "You have gone into too much debt. Facing no other options, you try getting a job.";
+	elif (ending == Ending.DEAD):
+		self.set_color(Color(0.21,0.01,0.01));
+		label.text = "The last of your appendages sliced off. What can you do on your own but die?";
+	self.show();
+	label.show();
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
